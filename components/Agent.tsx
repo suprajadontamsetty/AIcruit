304ed9827d357d323164ad0dbb72d3176c5cf3fb
Supraja <suprajadb@gmail.com>
2025-04-18 23:29:57 +0530

    show feedback

--- components/Agent.tsx
+++ components/Agent.tsx
@@ -6,6 +6,7 @@ import { vapi } from '@/lib/vapi.sdk';
 import { cn } from '@/lib/utils';
 import { useRouter } from 'next/navigation';
 import { interviewer } from '@/constants';
+import { createFeedback } from '@/lib/actions/general.action';
 
 enum CallStatus{
     INACTIVE='INACTIVE',
@@ -62,10 +63,11 @@ const Agent = ({userName,userId,type,interviewId, questions}:AgentProps) => {
       console.log('Generate feedback here.');
      
       //TODO:Create a server action that generates feedback
-      const {success,id}={
-        success:true,
-        id:'feedback-id'
-      }
+      const {success,feedbackId:id}=await createFeedback({
+        interviewId:interviewId!,
+        userId:userId!,
+        transcript:messages
+      })
 
       if(success && id) {
         router.push(`/interview/${interviewId}/feedback`);
